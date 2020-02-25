a11fa592007ba43b5d97e31e70e951d9037478c3
Jacob DeBenedetto <deben3@gmail.com>
2020-02-25 14:24:07 -0700

    add basic shortcode

--- functions.php
+++ functions.php
@@ -1,12 +1,15 @@
 <?php
+	// Queue child theme information
+	function theme_enqueue_styles() { wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('avada-stylesheet')); }
+	function avada_lang_setup() { $lang = get_stylesheet_directory() . '/languages'; load_child_theme_textdomain( 'Avada', $lang ); }
+	add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+	add_action('after_setup_theme', 'avada_lang_setup');
 
-function theme_enqueue_styles() {
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'avada-stylesheet' ) );
-}
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+	// Initialize shortcode object
+	require_once 'php/class-shortcodes.php';
+	$tfk_shortcodes = new TFK_Shortcodes();
+	$tfk_shortcodes -> run();
 
-function avada_lang_setup() {
-	$lang = get_stylesheet_directory() . '/languages';
-	load_child_theme_textdomain( 'Avada', $lang );
-}
-add_action( 'after_setup_theme', 'avada_lang_setup' );
+	// Initialize resources object
+	require_once 'php/class-resources.php';
+	$tfk_resources = new TFK_Resources();
---
+++ php/class-shortcodes.php
@@ -0,0 +1,49 @@
+<?php
+    class TFK_Shortcodes {
+        
+        public function __construct() {
+            
+        }
+
+        public function run(){
+            if (!is_admin()) {
+                add_shortcode('tfk', 'TFK_Shortcodes::shortcode');
+            }
+        }
+
+        public function shortcode($atts, $content = null) {
+            $id = $atts['id'];
+            $data = $atts['data'];
+            $type = $atts['type'];
+            $width = $atts['width'];
+            $height = $atts['height'];
+            $group = $atts['group'];
+
+            // Get post data
+            if (isset($id)) $post_id = $id;
+            else $post_id = get_the_ID();
+
+            // TEST
+            echo $post_id;
+
+            // Output custom fields by data type
+            if ($data == 'map') {
+                // Build style attribute
+                if (!empty($width) || !empty($height)) {
+                    if (!empty($width)) $width = 'width: ' . $width . '; ';
+                    if (!empty($height)) $height = 'height: ' . $height . '; ';
+                    $style = 'style="' . $width . $height . '"';
+                }
+
+                // Enqueue resources if map exists
+                wp_enqueue_script('leaflet');
+                wp_enqueue_style('leaflet');
+                wp_enqueue_script('leaflet-tfk');
+                wp_enqueue_style('leaflet-tfk');
+            }
+            else if ($data == 'lists' || $data == 'list') {
+
+            }
+        }
+    }
+?>
