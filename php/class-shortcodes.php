efa640d0136024d63e32879ecfde2809f0507e75
Jacob DeBenedetto <deben3@gmail.com>
2020-03-05 10:43:59 -0700

    add state group description

--- php/class-shortcodes.php
+++ php/class-shortcodes.php
@@ -56,6 +56,7 @@
             }
             else if ($data == 'list') {
                 // Generate array
+                $states = array('AL'=>'Alabama', 'AK'=>'Alaska', 'AZ'=>'Arizona', 'AR'=>'Arkansas', 'CA'=>'California', 'CO'=>'Colorado', 'CT'=>'Connecticut', 'DE'=>'Delaware', 'DC'=>'District of Columbia', 'FL'=>'Florida', 'GA'=>'Georgia', 'HI'=>'Hawaii', 'ID'=>'Idaho', 'IL'=>'Illinois', 'IN'=>'Indiana', 'IA'=>'Iowa', 'KS'=>'Kansas', 'KY'=>'Kentucky', 'LA'=>'Louisiana', 'ME'=>'Maine', 'MD'=>'Maryland', 'MA'=>'Massachusetts', 'MI'=>'Michigan', 'MN'=>'Minnesota', 'MS'=>'Mississippi', 'MO'=>'Missouri', 'MT'=>'Montana', 'NE'=>'Nebraska', 'NV'=>'Nevada', 'NH'=>'New Hampshire', 'NJ'=>'New Jersey', 'NM'=>'New Mexico', 'NY'=>'New York', 'NC'=>'North Carolina', 'ND'=>'North Dakota', 'OH'=>'Ohio', 'OK'=>'Oklahoma', 'OR'=>'Oregon', 'PA'=>'Pennsylvania', 'RI'=>'Rhode Island', 'SC'=>'South Carolina', 'SD'=>'South Dakota', 'TN'=>'Tennessee', 'TX'=>'Texas', 'UT'=>'Utah', 'VT'=>'Vermont', 'VA'=>'Virginia', 'WA'=>'Washington', 'WV'=>'West Virginia', 'WI'=>'Wisconsin', 'WY'=>'Wyoming');
                 $list_has_data = false;
                 $acf_group_key = 'Location';
                 $acf_meta_key = 'general';
@@ -91,10 +92,13 @@
                     $group_start = '';
                     $group_end = '';
                     $group_index++;
+
+                    // Convert state abbreviation
+                    if ($group == 'state') $group_toggle = $states[$group_key];
                     
                     // Define group wrapper
                     if (!empty($group)) {
-                        $group_start = '<li class="group"><a class="group-title" aria-selected="false" href="#"><span class="icon"></span>' . $group_toggle . '</a><ul class="group-content">';
+                        $group_start = '<li class="group ' . $group . '"><a class="group-title" aria-selected="false" href="#"><span class="icon"></span>' . $group_toggle . '</a><ul class="group-content">';
                         $group_end = '</ul></li>';
                     }
 
