ad75f9ffe03a775f5bb0b182defd5c825e129d5c
Jacob DeBenedetto <deben3@gmail.com>
2020-03-16 10:11:02 -0700

    add slide animations

--- js/scripts-tfk.js
+++ js/scripts-tfk.js
@@ -11,8 +11,31 @@
 
 	// Add slider functionality
 	$(document).ready(function(){
-		if ($('.tfk-slider').length) {
-			$('.tfk-slider').slick({ dots: true, speed: 1000, fade: true });
+		var slider = $('.tfk-slider');
+		if (slider.length) {
+			slider.on('init', function(event, slick) {
+				// Add pause toggle button
+				var slideStateHTML = $('<a href="#" class="slider-toggle" aria-label="pause">pause</a>');
+				$(this).addClass('animating');
+				$('.slick-dots').append(slideStateHTML);
+				slideStateHTML.on('click', function(e) {
+					e.preventDefault();
+					var slideState = slideStateHTML.text();
+					var slideStateOption = (slideState == "play") ? "pause" : "play";
+					slideStateHTML.attr('aria-label', slideStateOption);
+					slideStateHTML.text(slideStateOption);
+					if (slideState == 'play') slider.slick('slickPlay');
+					else slider.slick('slickPause');
+				});
+			});
+			slider.on('beforeChange', function(event, slick){
+				slider.removeClass('animating');
+				setTimeout(function(){ slider.addClass('animating'); }, 50);
+			});
+			slider.on('afterChange', function(event, slick){
+				
+			});
+			slider.slick({ autoplay: true, dots: true, speed: 1000, fade: true });
 		}
 	});
 	
