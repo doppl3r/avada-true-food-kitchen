43280682e8cd6e8da3ba397d49d75548a7d5ea1f
Jacob DeBenedetto <deben3@gmail.com>
2020-04-23 17:32:54 -0700

    add momentfeed location integration

--- js/momentfeed-menu-v2.js
+++ js/momentfeed-menu-v2.js
@@ -8,30 +8,26 @@
     function renderMomentfeedMenus(location_id) {
         var proxy = '';
         var apiUrl = "https://momentfeed-prod.apigee.net/menu/v1/location/" + location_id+ "/menus?auth_token=" + token;
-        //var apiUrl = "https://momentfeed-prod.apigee.net/menu/account/" + accountId + "/menus?auth_token=" + token;
         $('#menu_tabs').addClass('loading');
         if (window.location.href.includes('truefoodkitchen.com') == false) proxy = 'https://cors-anywhere.herokuapp.com/';
         $.getJSON(proxy + apiUrl).done(
             function (data) {
                 $('#menu_tabs').removeClass('loading');
-                menus = data.data;
-                $.each(menus, function (i, e) { handleMenu(menus[i], i); });
-            }).fail(function (xhr, status, error) { console.error("error: " + xhr.responseText); });
+                var sections = data.data[0].sections;
+                $.each(sections, function (i, e) { handleMenu(sections[i], i); });
+            }).fail(function (xhr, status, error) { console.error("error: " + xhr.responseText);
+        });
     }
-    function handleMenu(menu, menuId) {
-        $("ul#menu_tabs").append('<li class="primary_menu_tab ' + (tabIndex === 0 ? "active" : "inactive") + '" role="tab" aria-selected="' + (tabIndex === 0 ? "true" : "false") + '" rel="' + menuId + '" tabindex="' + tabIndex + '" aria-controls="' + menuId + '">' + menu.displayName + '</li > ');
-        $("#output").append('<div id="' + menuId + '" class="tabpanel active row" role="tabpanel" aria-hidden="' + (tabIndex === 0 ? "true" : "false") + '" style="' + (tabIndex === 0 ? "display:block" : "display:none") + '" ></div>');
-        $("#" + menuId).append("<fieldset class='category_root' id='menu-" + menuId + "' style='padding:2px;'>" + "<legend>" + menu.displayName + "</legend>" + "<div class='category_sections'></div>" + "</fieldset>");
+    function handleMenu(section, sectionIndex) {
+        $("ul#menu_tabs").append('<li class="primary_menu_tab ' + (tabIndex === 0 ? "active" : "inactive") + '" role="tab" aria-selected="' + (tabIndex === 0 ? "true" : "false") + '" rel="' + sectionIndex + '" tabindex="' + tabIndex + '" aria-controls="' + sectionIndex + '">' + section.displayName + '</li > ');
+        $("#output").append('<div id="' + sectionIndex + '" class="tabpanel active row" role="tabpanel" aria-hidden="' + (tabIndex === 0 ? "true" : "false") + '" style="' + (tabIndex === 0 ? "display:block" : "display:none") + '" ></div>');
+        $("#" + sectionIndex).append("<fieldset class='category_root' id='menu-" + sectionIndex + "' style='padding:2px;'>" + "<legend>" + section.displayName + "</legend>" + "<div class='category_sections'></div>" + "</fieldset>");
+        $("#menu-" + sectionIndex + " .category_sections").append("<div class='col-md-12 col-sm-12 pb-2'>" + "<h3 class='section_name' style='color:#487426; font-size:18px; font-weight:600; margin:4px; padding:4px;'>" + section.displayName + "</h3>" + "<ul id='section-" + sectionIndex + "' class='section_items'>" + "</ul>" + "</div>");
+        $.each(section.items, function (i, e) { handleItem(section.items[i], sectionIndex); });
         tabIndex = -1;
-        $.each(menu.sections, function (i, e) { handleSection(menu.sections[i], i); });
     }
-    function handleSection(section, menuId) {
-        console.log(section);
-        $("#menu-" + menuId + " .category_sections").append("<div class='col-md-12 col-sm-12 pb-2'>" + "<h3 class='section_name' style='color:#487426; font-size:18px; font-weight:600; margin:4px; padding:4px;'>" + section.displayName + "</h3>" + "<p class='small'>" + section.description + "</p>" + "<ul id='section-" + section.id + "' class='section_items'>" + "</ul>" + "</div>");
-        $.each(section.items, function (i, e) { handleItem(section.items[i], section.id); });
-    }
-    function handleItem(item, sectionId) {
-        $("#section-" + sectionId + "").append("<li>" + "<div class='item_container'>" + "<p class='item_name'>" + (item.displayName.indexOf('**') == 0 ? "<img id='seasonal-img' src='/wp-content/uploads/2020/03/icon_menu_seasonal.png'>" : "") + item.displayName.replace('**', '') + "</p>" + ((item.price.amount === "") ? "" : "<p>$" + item.price.amount + "</p>") + "</div>" + "<p class='item_description'>" + item.description + "</p>" + "</li>");
+    function handleItem(item, sectionIndex) {
+        $("#section-" + sectionIndex + "").append("<li>" + "<div class='item_container'>" + "<p class='item_name'>" + (item.displayName.indexOf('**') == 0 ? "<img id='seasonal-img' src='/wp-content/uploads/2020/03/icon_menu_seasonal.png'>" : "") + item.displayName.replace('**', '') + "</p>" + ((item.price.amount === "") ? "" : "<p>$" + item.price.amount + "</p>") + "</div>" + "<p class='item_description'>" + item.description + "</p>" + "</li>");
     }
     $('#menu_tabs').on('keydown', function (e) {
         if (e.which === 37 || e.which === 38 || (e.which === 33 && e.ctrlKey)) { //left/up/ctrl+pageup
